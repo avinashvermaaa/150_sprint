92de30a688617be8aa1125622e3d764e7941ccce
Avinash <code6969nation@gmail.com>
2025-03-25 04:04:11 +0530

    19 prime numbers in a range added

---
+++ PS_1/19 prime number in a range.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+
+bool isprime(int n){
+	if(n <= 1) return false;
+	if(n <= 3) return true;
+	
+	if(n%2 == 0 || n%3 ==0) return false;
+	
+	for(int i = 6;i*i<=n; i+= 6){
+		if( (n % (i-1) == 0) || (n% (i+1)==0) ) return false;
+	}
+	
+	return true;
+}
+
+void find_prime(int a, int b){
+	
+	vector<int> primes;
+	
+	for(int i = a; i<=b; i++){
+		if(isprime(i) ) primes.push_back(i);
+	}
+	
+	for(auto it : primes){
+		cout<<it<<" ";
+	}cout<<endl;
+
+	
+}
+
+
+int main() {
+	
+	int a,b;
+	cin>>a>>b;
+	
+	find_prime(a,b);
+    
+    return 69;
+}
