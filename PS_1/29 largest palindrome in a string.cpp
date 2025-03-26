a71b35baf797e724ddc6433c3ce88b2ac3a7d7e0
Avinash <code6969nation@gmail.com>
2025-03-27 01:18:56 +0530

    largest palindrome in a string added

---
+++ PS_1/29 largest palindrome in a string.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+bool check_palindrome(string str){
+	
+	int n = str.size();
+	int last = n-1;
+	
+	for(int i = 0;i<n;i++){
+		if(str[i] == str[last-i]) continue;
+		else return false;
+	}
+	
+	return true;
+}
+
+void get_largest_plaindrome(string str){
+	
+	int n = str.size(), max_size = 0;
+	string largest_string = "";
+	
+	for(int i = 0;i<n;i++){
+		string word = "";
+		for(int j = i;j<n;j++){
+			word += str[j];
+			if(check_palindrome(word)){
+				int curr_size = word.size();
+				if(curr_size >= max_size){
+					max_size = curr_size;
+				  	largest_string = word;
+				}
+			}
+		}
+	}
+	
+	cout<<largest_string<<endl;
+	
+}
+
+int main() {
+	
+	string str;
+	cin>>str;
+	
+	get_largest_plaindrome(str);
+    
+    return 69;
+}
