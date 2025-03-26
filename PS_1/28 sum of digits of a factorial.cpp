c66fc4424c40e5564f48e49169791f469fa57c9b
Avinash <code6969nation@gmail.com>
2025-03-26 19:56:38 +0530

    28 sum of digits of a factorial added

---
+++ PS_1/28 sum of digits of a factorial.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int factorial(int n){
+	
+	if(n <= 1) return 1;
+	
+	int facto = 1;
+	
+	for(int i = 1;i<=n;i++){
+		facto = facto * i;
+	}
+	
+	return facto;
+}
+
+
+void sum_of_digits(int n){
+	
+	int facto = factorial(n); 
+	int sum = 0;
+	
+	while(facto){
+		int unit = facto % 10;
+		
+		sum += unit;
+		
+		facto /= 10;
+	}
+	
+	cout<<sum<<endl;
+	
+}
+
+int main() {
+	
+	int n;
+	cin>>n;
+	
+	sum_of_digits(n);
+    
+    return 69;
+}
