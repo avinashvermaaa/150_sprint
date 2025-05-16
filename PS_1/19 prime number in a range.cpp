bc129fb17fc3e3a914f0b0d49272b35913c14da2
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2025-05-16 09:08:57 +0530

    Upd : corrected code for 25

--- PS_1/19 prime number in a range.cpp
+++ PS_1/19 prime number in a range.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 
-bool isprime(int n){
-	if(n <= 1) return false;
-	if(n <= 3) return true;
-	
-	if(n%2 == 0 || n%3 ==0) return false;
-	
-	for(int i = 6;i*i<=n; i+= 6){
-		if( (n % (i-1) == 0) || (n% (i+1)==0) ) return false;
-	}
-	
-	return true;
+bool isprime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+
+    return true;
 }
 
 void find_prime(int a, int b){
